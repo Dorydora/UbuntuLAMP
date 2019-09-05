3e976a92a6103b9852b8d3f33b811ebb8ad0b222
Dorydora <dorydorma@hotmail.com>
2019-09-05 11:08:49 +1000

    commit

---
+++ qryTableCust.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CUSTOMERS
+(
+        CustID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(48),
+    SurName VARCHAR(48),
+    Street VARCHAR(48),
+    City VARCHAR(32),
+    State VARCHAR(3),
+    Postcode VARCHAR(5),
+);
