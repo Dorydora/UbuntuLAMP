6efdf709e5e004b80d069dba8d6b88d6855bcea0
Dorydora <dorydorma@hotmail.com>
2019-10-28 13:34:00 +1100

    commit

--- PHP/SQL CON/table.php
+++ PHP/SQL CON/table.php
@@ -7,7 +7,14 @@ if (mysqli_connect_errno()) {
 }
 
 // Create table
-$sql="CREATE TABLE Persons(FirstName CHAR(30),LastName CHAR(30),Age INT)";
+$sql="CREATE TABLE Persons
+(
+    PID INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(PID),
+    FirstName CHAR(15),
+    LastName CHAR(15),
+    Age INT,
+)";
 
 // Execute query
 if (mysqli_query($con,$sql)) {
