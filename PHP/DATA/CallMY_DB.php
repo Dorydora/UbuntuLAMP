b3eb3099320bafe8a60ae95a4fc11916ab2eb133
Dorydora <dorydorma@hotmail.com>
2019-10-31 10:22:57 +1100

    commit

--- PHP/DATA/CallMY_DB.php
+++ PHP/DATA/CallMY_DB.php
@@ -3,9 +3,10 @@ error_reporting(E_ALL);
 ini_set('display_errors',1);
 
 $con=mysqli_connect("localhost","admin","Password1!","my_db");
+
 // Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 $result = mysqli_query($con,"SELECT * FROM Persons");
