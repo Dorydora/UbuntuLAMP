349fbd55ea8329037cae6be3061265958fff1e54
Dorydora <dorydorma@hotmail.com>
2019-10-31 10:31:34 +1100

    commit

--- PHP/SQL CON/insert.php
+++ PHP/SQL CON/insert.php
@@ -14,7 +14,7 @@ $lastname = mysqli_real_escape_string($con, $_POST['lastname']);
 $age = mysqli_real_escape_string($con, $_POST['age']);
 
 $sql="INSERT INTO Persons (FirstName, LastName, Age)
-VALUES ('$firstname', '$lastname', '$age')";
+VALUES ('$firstname', '$lastname', '$age')"
 
 if (!mysqli_query($con,$sql)) {
     die('Error: ' . mysqli_error($con));
