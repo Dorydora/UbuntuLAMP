140c435567272ac6f9ed07824301aebf1ab18852
Dorydora <dorydorma@hotmail.com>
2019-10-31 10:51:51 +1100

    commit

--- PHP/DATA/CallMy_DBTable.php
+++ PHP/DATA/CallMy_DBTable.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
 $con=mysqli_connect("localhost","admin","Password1!","my_db");
 
 // Check connection
@@ -8,11 +11,7 @@ if (mysqli_connect_errno()){
 
 $result = mysqli_query($con,"SELECT * FROM Persons");
 
-echo "<table border='1'>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>";
+echo "<table border=1> <tr> <th>Firstname</th> <th>Lastname</th> </tr>";
 
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
