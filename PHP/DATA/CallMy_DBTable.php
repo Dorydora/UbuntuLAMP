c1b04ade8218e773bef06eafb8f7f08b37520d87
Dorydora <dorydorma@hotmail.com>
2019-10-31 11:02:08 +1100

    commit

--- PHP/DATA/CallMy_DBTable.php
+++ PHP/DATA/CallMy_DBTable.php
@@ -1,23 +1,23 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors',1);
-
-$con=mysqli_connect("localhost","admin","Password1!","my_db");
-
+$con=mysqli_connect("localhost","root","","my_db");
 // Check connection
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysql_connect_errno()){
+    echo"Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 $result = mysqli_query($con,"SELECT * FROM Persons");
 
-echo "<table border=1> <tr> <th>Firstname</th> <th>Lastname</th> </tr>";
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
 
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['FirstName'] . "</td>";
-  echo "<td>" . $row['LastName'] . "</td>";
-  echo "</tr>";
+while($row = mysqli_fetch_array($result)){
+    echo "<tr>";
+    echo "<td>" . $row['FirstName'] . "</td>";
+    echo "<td>" . $row['LastName'] . "</td>";
+    echo "</tr>";
 }
 
 echo "</table>";
