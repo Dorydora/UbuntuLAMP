a9c0b6fbef44b4538f9f146c45afc5e16043f78d
Dorydora <dorydorma@hotmail.com>
2019-11-20 12:06:24 +1100

    commit

--- PHP/MAJOR ASSESSMENT/Styles/adminIndex.php
+++ PHP/MAJOR ASSESSMENT/Styles/adminIndex.php
@@ -4,6 +4,6 @@
 <a href = "adminLogin.php"> Login </a></br>
 <a href = "logout.php"> Logout </a></br>
 
-<a href = "addPart.php"> Add A Part</a>
+<a href = "addPart.php"> Add A Part</a></br>
 <a href = "changeParts2.php"> Update A Part</a>
 </html>
